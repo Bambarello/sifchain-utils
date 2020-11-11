435a06e65a2eb40014ef5509b9a16e91968b0725
Alex <33266159+Bambarello@users.noreply.github.com>
2020-11-11 10:51:58 +0200

    Update redelegate.sh

--- redelegate.sh
+++ redelegate.sh
@@ -2,9 +2,10 @@
 
 BIN_FILE="/root/go/bin/sifnodecli"
 RPC_PORT="27657"
+PASS="XXXXXXXXXXXX"
 WALLET_NAME="sifchain"
 CHAIN_ID="monkey-bars"
-SELF_ADDR=$($BIN_FILE keys list -o json | jq -r .[0].address)
+SELF_ADDR=$(echo $PASS | $BIN_FILE keys list -o json | jq -r .[0].address)
 DENOM="rowan"
 OPERATOR=$($BIN_FILE q staking delegations --chain-id $CHAIN_ID -o json --node tcp://localhost:$RPC_PORT $SELF_ADDR | jq -r .[].validator_address)
 
@@ -19,7 +20,7 @@ do
     if (( $BALANCE >  3000000 )); then
         echo "Let's delegate $REWARD of REWARD tokens to $SELF_ADDR"
         # delegate balance
-        $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --node tcp://localhost:$RPC_PORT --gas-adjustment 1.5 --gas="200000" --fees 7500$DENOM --from $WALLET_NAME -y
+        echo -e "$PASS\n$PASS\n" | $BIN_FILE tx staking delegate $OPERATOR "$REWARD"$DENOM --chain-id $CHAIN_ID --node tcp://localhost:$RPC_PORT --gas-adjustment 1.5 --gas="200000" --fees 7500$DENOM --from $WALLET_NAME -y
 
     else
         echo "Reward is $REWARD"
